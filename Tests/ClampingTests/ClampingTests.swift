057f035a3888f357a447b2fae577a88ddc996d69
Edon <edon@valdman.works>
2024-05-20 13:08:38 -0500

    Update ClampingTests.swift

--- Tests/ClampingTests/ClampingTests.swift
+++ Tests/ClampingTests/ClampingTests.swift
@@ -2,11 +2,38 @@ import XCTest
 @testable import Clamping
 
 final class ClampingTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    let closedRange = -12...25
+    let partialRangeFrom = (-34)...
+    let partialRangeThrough = ...25
+    
+    func testClosedRange() throws {
+        let testValues = [-50, -13, -12, -3, 0, 3, 25, 26, 50]
+        let resultValues = [-12, -12, -12, -3, 0, 3, 25, 25, 25]
+        
+        for (test, result) in zip(testValues, resultValues) {
+            XCTAssert(closedRange.clamp(test) == result, "\(closedRange) clamping \(test) did not equal expected result: \(result).")
+            XCTAssert(test.clamped(to: closedRange) == result, "\(test) clamped to \(closedRange) did not equal expected result: \(result).")
+        }
+    }
+    
+    func testPartialRangeFrom() throws {
+        let testValues = [-50, -35, -34, -3, 0, 3, 25, 26, 50]
+        let resultValues = [-34, -34, -34, -3, 0, 3, 25, 26, 50]
+        
+        for (test, result) in zip(testValues, resultValues) {
+            XCTAssert(partialRangeFrom.clamp(test) == result, "\(partialRangeFrom) clamping \(test) did not equal expected result: \(result).")
+            XCTAssert(test.clamped(to: partialRangeFrom) == result, "\(test) clamped to \(partialRangeFrom) did not equal expected result: \(result).")
+        }
+    }
+    
+    func testPartialRangeThrough() throws {
+        let testValues = [-50, -35, -34, -3, 0, 3, 25, 26, 50]
+        let resultValues = [-50, -35, -34, -3, 0, 3, 25, 25, 25]
+        
+        for (test, result) in zip(testValues, resultValues) {
+            XCTAssert(partialRangeThrough.clamp(test) == result, "\(partialRangeThrough) clamping \(test) did not equal expected result: \(result).")
+            XCTAssert(test.clamped(to: partialRangeThrough) == result, "\(test) clamped to \(partialRangeThrough) did not equal expected result: \(result).")
+        }
     }
 }
